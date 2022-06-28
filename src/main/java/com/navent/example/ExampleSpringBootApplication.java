28780dc10ae4e3894d2f6f43dce096049de55949
vecruz1986 <vcruz@navent.com>
2022-06-28 16:19:25 -0300

    mi primer commit

--- src/main/java/com/navent/example/ExampleSpringBootApplication.java
+++ src/main/java/com/navent/example/ExampleSpringBootApplication.java
@@ -2,19 +2,19 @@ package com.navent.example;
 
 import com.navent.example.model.Posting;
 import com.navent.example.reader.ExampleFileReader;
+import com.navent.example.services.PostingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.util.List;
-
 @SpringBootApplication
 public class ExampleSpringBootApplication implements CommandLineRunner {
-
 	@Autowired
 	private ExampleFileReader reader;
 
+	@Autowired
+	private PostingService ps;
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringBootApplication.class, args);
 	}
@@ -23,5 +23,8 @@ public class ExampleSpringBootApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		List<Posting> postings = reader.getPostingFromJson();
 		System.out.println(postings);
+		Posting posting = ps.getPostingOfPriceMin();
+		System.out.println(posting);
 	}
+
 }
