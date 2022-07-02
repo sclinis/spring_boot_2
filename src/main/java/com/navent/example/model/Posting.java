480dcec1f5bea72eb12300bd63defeff7e112b4e
Alejo Aseijas <aaseijas@navent.com>
2022-07-02 20:02:49 -0300

    Delete @Component in Posting Model.

--- src/main/java/com/navent/example/model/Posting.java
+++ src/main/java/com/navent/example/model/Posting.java
@@ -2,10 +2,8 @@ package com.navent.example.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 public class Posting {
     @JsonProperty("title")
     private String title;
